576e219a2fbc01271489247092d52b7804539073
Wakana <wakaichi@gmail.com>
2023-03-24 12:52:13 +0400

    fixed bugs

---
+++ index.js
@@ -0,0 +1,70 @@
+function myFunction() {
+    var x = document.getElementById("answer");
+    var y = document.getElementById("p");
+    var z = document.getElementById("arrow1")
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.classList.add("fWeight");
+      z.style.transform = 'rotate(180deg)';
+    } else {
+      x.style.display = "none";
+      y.classList.remove("fWeight");
+      z.style.transform = 'rotate(360deg)';
+    }
+  }
+  function myFunction1() {
+    var x = document.getElementById("answer1");
+    var y = document.getElementById("p1");
+    var z = document.getElementById("arrow2")
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.classList.add("fWeight");
+      z.style.transform = 'rotate(180deg)';
+    } else {
+      x.style.display = "none";
+      y.classList.remove("fWeight");
+      z.style.transform = 'rotate(360deg)';
+    }
+  }
+  function myFunction2() {
+    var x = document.getElementById("answer2");
+    var y = document.getElementById("p2");
+    var z = document.getElementById("arrow3")
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.classList.add("fWeight");
+      z.style.transform = 'rotate(180deg)';
+    } else {
+      x.style.display = "none";
+      y.classList.remove("fWeight");
+      z.style.transform = 'rotate(360deg)';
+    }
+  }
+  function myFunction3() {
+    var x = document.getElementById("answer3");
+    var y = document.getElementById("p3");
+    var z = document.getElementById("arrow4")
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.classList.add("fWeight");
+      z.style.transform = 'rotate(180deg)';
+    } else {
+      x.style.display = "none";
+      y.classList.remove("fWeight");
+      z.style.transform = 'rotate(360deg)';
+    }
+  }
+  function myFunction4() {
+    var x = document.getElementById("answer4");
+    var y = document.getElementById("p4");
+    var z = document.getElementById("arrow5")
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.classList.add("fWeight");
+      z.style.transform = 'rotate(180deg)';
+    } else {
+      x.style.display = "none";
+      y.classList.remove("fWeight");
+      z.style.transform = 'rotate(360deg)';
+    }
+  }
